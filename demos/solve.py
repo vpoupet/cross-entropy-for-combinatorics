54bef38b93b36de0bff6ad258527f2ae7428e422
vpoupet <victor.poupet@gmail.com>
2022-04-28 11:39:57 +0200

    efficient elite select (and renaming)

--- demos/solve.py
+++ demos/solve.py
@@ -16,8 +16,10 @@ MYN = int(N * (N - 1) / 2)
 # Increase this to make convergence faster, decrease if the algorithm gets stuck in local optima too often.
 LEARNING_RATE = 0.0001
 BATCH_SIZE = 1000  # number of new sessions per iteration
-PERCENTILE = 93  # top 100-X percentile we are learning from
+ELITE_PERCENTILE = 93  # top 100-X percentile we are learning from
 SUPER_PERCENTILE = 94  # top 100-X percentile that survives to next iteration
+NB_ELITE = int(BATCH_SIZE * (100 - ELITE_PERCENTILE) / 100)
+NB_SUPER = int(BATCH_SIZE * (100 - SUPER_PERCENTILE) / 100)
 
 FIRST_LAYER_NEURONS = 128  # Number of neurons in the hidden layers.
 SECOND_LAYER_NEURONS = 64
@@ -60,7 +62,7 @@ def nb_components(g):
     return np.linalg.eigvals(laplacian).count(0)
 
 
-def calcScore(state):
+def get_reward(state):
     """
     Calculates the reward for a given word.
     This function is very slow, it can be massively sped up with numba -- but numba doesn't support networkx yet, which is very convenient to use here
@@ -103,27 +105,27 @@ def calcScore(state):
 
     # Calculate the reward. Since we want to minimize lambda_1 + mu, we return the negative of this.
     # We add to this the conjectured best value. This way if the reward is positive we know we have a counterexample.
-    myScore = math.sqrt(N - 1) + 1 - lambda1 - mu
-    if myScore > 0:
+    reward = math.sqrt(N - 1) + 1 - lambda1 - mu
+    if reward > 0:
         # You have found a counterexample. Do something with it.
         print(state)
         nx.draw_kamada_kawai(g)
         plt.show()
         exit()
 
-    return myScore
+    return reward
 
 
 # No need to change anything below here.
 
 
-def generate_session(model, batch_size):
+def run_batch(model, batch_size):
     states = np.zeros((batch_size, GAME_LENGTH, STATE_SIZE), dtype=int)
     actions = np.zeros([batch_size, GAME_LENGTH], dtype=int)
     prob = np.zeros(batch_size)
     step_i = 0
     step_j = 1
-    score = np.zeros((batch_size,))
+    rewards = np.zeros((batch_size,))
 
     for step in range(GAME_LENGTH):
         states[:, step, MYN + step_i] = 1
@@ -142,43 +144,12 @@ def generate_session(model, batch_size):
             step_j = step_i + 1
 
     for i in range(batch_size):
-        score[i] = calcScore(states[i, GAME_LENGTH - 1, :])
+        rewards[i] = get_reward(states[i, GAME_LENGTH - 1, :])
 
-    return states, actions, score
-
-
-def select_elites(states_batch, actions_batch, rewards_batch, percentile=50):
-    """
-    Select states and actions from games that have rewards >= percentile
-    :param states_batch: list of lists of states, states_batch[session_i][t]
-    :param actions_batch: list of lists of actions, actions_batch[session_i][t]
-    :param rewards_batch: list of rewards, rewards_batch[session_i]
-
-    :returns: elite_states,elite_actions, both 1D lists of states and respective actions from elite sessions
-
-    This function was mostly taken from https://github.com/yandexdataschool/Practical_RL/blob/master/week01_intro/deep_crossentropy_method.ipynb
-    If this function is the bottleneck, it can easily be sped up using numba
-    """
-    counter = BATCH_SIZE * (100.0 - percentile) / 100.0
-    reward_threshold = np.percentile(rewards_batch, percentile)
-
-    elite_states = []
-    elite_actions = []
-    elite_rewards = []
-    for i in range(len(states_batch)):
-        if rewards_batch[i] >= reward_threshold - 0.0000001:
-            if (counter > 0) or (rewards_batch[i] >= reward_threshold + 0.0000001):
-                for item in states_batch[i]:
-                    elite_states.append(item.tolist())
-                for item in actions_batch[i]:
-                    elite_actions.append(item)
-            counter -= 1
-    elite_states = np.array(elite_states, dtype=int)
-    elite_actions = np.array(elite_actions, dtype=int)
-    return elite_states, elite_actions
+    return states, actions, rewards
 
 
-def select_super_sessions(states_batch, actions_batch, rewards_batch, percentile=90):
+def select_super_sessions(states, actions, rewards, percentile=90):
     """
     Select all the sessions that will survive to the next generation
     Similar to select_elites function
@@ -186,17 +157,17 @@ def select_super_sessions(states_batch, actions_batch, rewards_batch, percentile
     """
 
     counter = BATCH_SIZE * (100.0 - percentile) / 100.0
-    reward_threshold = np.percentile(rewards_batch, percentile)
+    reward_threshold = np.percentile(rewards, percentile)
 
     super_states = []
     super_actions = []
     super_rewards = []
-    for i in range(len(states_batch)):
-        if rewards_batch[i] >= reward_threshold - 0.0000001:
-            if (counter > 0) or (rewards_batch[i] >= reward_threshold + 0.0000001):
-                super_states.append(states_batch[i])
-                super_actions.append(actions_batch[i])
-                super_rewards.append(rewards_batch[i])
+    for i in range(len(states)):
+        if rewards[i] >= reward_threshold - 0.0000001:
+            if (counter > 0) or (rewards[i] >= reward_threshold + 0.0000001):
+                super_states.append(states[i])
+                super_actions.append(actions[i])
+                super_rewards.append(rewards[i])
                 counter -= 1
     super_states = np.array(super_states, dtype=int)
     super_actions = np.array(super_actions, dtype=int)
@@ -204,80 +175,77 @@ def select_super_sessions(states_batch, actions_batch, rewards_batch, percentile
     return super_states, super_actions, super_rewards
 
 
-super_states = np.empty((0, GAME_LENGTH, STATE_SIZE), dtype=int)
-super_actions = np.array([], dtype=int)
-super_rewards = np.array([])
-sessgen_time = 0
-fit_time = 0
-score_time = 0
+if __name__ == "__main__":
+    super_states = np.empty((0, GAME_LENGTH, STATE_SIZE), dtype=int)
+    super_actions = np.array([], dtype=int)
+    super_scores = np.array([])
+
+    myRand = random.randint(0, 1000)  # used in the filename
+
+    for i in range(1000000):  # 1000000 generations should be plenty
+        # generate new sessions
+        # performance can be improved with joblib
+        tic = time.time()
+        states, actions, rewards = run_batch(model, BATCH_SIZE)
+
+        states = np.append(states, super_states, axis=0)
+        if i > 0:
+            actions = np.append(actions, np.array(super_actions), axis=0)
+        rewards = np.append(rewards, super_scores)
+
+        # select elites (sessions to learn from)
+        elite_indexes = np.argpartition(rewards, -NB_ELITE)[-NB_ELITE:]
+        elite_states = np.concatenate(states[elite_indexes])
+        elite_actions = np.concatenate(actions[elite_indexes])
 
 
-myRand = random.randint(0, 1000)  # used in the filename
+        super_sessions = select_super_sessions(
+            states, actions, rewards, percentile=SUPER_PERCENTILE
+        )  # pick the sessions to survive
 
-for i in range(1000000):  # 1000000 generations should be plenty
-    # generate new sessions
-    # performance can be improved with joblib
-    tic = time.time()
-    # change 0 to 1 to print out how much time each step in generate_session takes
-    sessions = generate_session(model, BATCH_SIZE)
+        super_sessions = [
+            (super_sessions[0][i], super_sessions[1][i], super_sessions[2][i])
+            for i in range(len(super_sessions[2]))
+        ]
+        super_sessions.sort(key=lambda super_sessions: super_sessions[2], reverse=True)
+        select3_time = time.time() - tic
 
-    states_batch = np.array(sessions[0], dtype=int)
-    actions_batch = np.array(sessions[1], dtype=int)
-    rewards_batch = np.array(sessions[2])
-    # states_batch = np.transpose(states_batch, axes=[0, 2, 1])
+        model.fit(elite_states, elite_actions)  # learn from the elite sessions
 
-    states_batch = np.append(states_batch, super_states, axis=0)
+        super_states = [super_sessions[i][0] for i in range(len(super_sessions))]
+        super_actions = [super_sessions[i][1] for i in range(len(super_sessions))]
+        super_scores = [super_sessions[i][2] for i in range(len(super_sessions))]
 
-    if i > 0:
-        actions_batch = np.append(actions_batch, np.array(super_actions), axis=0)
-    rewards_batch = np.append(rewards_batch, super_rewards)
+        rewards.sort()
+        mean_all_reward = np.mean(rewards[-100:])
+        mean_best_reward = np.mean(super_scores)
 
-    elite_states, elite_actions = select_elites(
-        states_batch, actions_batch, rewards_batch, percentile=PERCENTILE
-    )  # pick the sessions to learn from
+        print(
+            "\n"
+            + str(i)
+            + ". Best individuals: "
+            + str(np.flip(np.sort(super_scores)))
+        )
 
-    super_sessions = select_super_sessions(
-        states_batch, actions_batch, rewards_batch, percentile=SUPER_PERCENTILE
-    )  # pick the sessions to survive
+        # uncomment below line to print out how much time each step in this loop takes.
+        print(f"Mean reward: {mean_all_reward}, time: {time.time() - tic}")
 
-    super_sessions = [
-        (super_sessions[0][i], super_sessions[1][i], super_sessions[2][i])
-        for i in range(len(super_sessions[2]))
-    ]
-    super_sessions.sort(key=lambda super_sessions: super_sessions[2], reverse=True)
-    select3_time = time.time() - tic
-
-    model.fit(elite_states, elite_actions)  # learn from the elite sessions
-
-    super_states = [super_sessions[i][0] for i in range(len(super_sessions))]
-    super_actions = [super_sessions[i][1] for i in range(len(super_sessions))]
-    super_rewards = [super_sessions[i][2] for i in range(len(super_sessions))]
-
-    rewards_batch.sort()
-    mean_all_reward = np.mean(rewards_batch[-100:])
-    mean_best_reward = np.mean(super_rewards)
-
-    print("\n" + str(i) + ". Best individuals: " + str(np.flip(np.sort(super_rewards))))
-
-    # uncomment below line to print out how much time each step in this loop takes.
-    print(f"Mean reward: {mean_all_reward}, time: {time.time() - tic}")
-
-    if i % 20 == 1:  # Write all important info to files every 20 iterations
-        with open("best_species_pickle_" + str(myRand) + ".txt", "wb") as fp:
-            pickle.dump(super_actions, fp)
-        with open("best_species_txt_" + str(myRand) + ".txt", "w") as f:
-            for item in super_actions:
-                f.write(str(item))
+        if i % 20 == 1:  # Write all important info to files every 20 iterations
+            with open("best_species_pickle_" + str(myRand) + ".txt", "wb") as fp:
+                pickle.dump(super_actions, fp)
+            with open("best_species_txt_" + str(myRand) + ".txt", "w") as f:
+                for item in super_actions:
+                    f.write(str(item))
+                    f.write("\n")
+            with open("best_species_rewards_" + str(myRand) + ".txt", "w") as f:
+                for item in super_scores:
+                    f.write(str(item))
+                    f.write("\n")
+            with open("best_100_rewards_" + str(myRand) + ".txt", "a") as f:
+                f.write(str(mean_all_reward) + "\n")
+            with open("best_elite_rewards_" + str(myRand) + ".txt", "a") as f:
+                f.write(str(mean_best_reward) + "\n")
+        if i % 200 == 2:  # To create a timeline, like in Figure 3
+            with open("best_species_timeline_txt_" + str(myRand) + ".txt", "a") as f:
+                f.write(str(super_actions[0]))
                 f.write("\n")
-        with open("best_species_rewards_" + str(myRand) + ".txt", "w") as f:
-            for item in super_rewards:
-                f.write(str(item))
-                f.write("\n")
-        with open("best_100_rewards_" + str(myRand) + ".txt", "a") as f:
-            f.write(str(mean_all_reward) + "\n")
-        with open("best_elite_rewards_" + str(myRand) + ".txt", "a") as f:
-            f.write(str(mean_best_reward) + "\n")
-    if i % 200 == 2:  # To create a timeline, like in Figure 3
-        with open("best_species_timeline_txt_" + str(myRand) + ".txt", "a") as f:
-            f.write(str(super_actions[0]))
-            f.write("\n")
