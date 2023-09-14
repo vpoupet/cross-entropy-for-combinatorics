4e4e369ec802f998c1cce1dfbccebbc4295ec84c
Victor Poupet <victor.poupet@gmail.com>
2023-09-14 14:41:20 +0200

    New utils

--- utils.py
+++ utils.py
@@ -6,6 +6,10 @@ EPSILON = 1e-10
 INF = float("inf")
 
 
+def get_nb_vertices(nb_edges: int) -> int:
+    return int((1 + np.sqrt(1 + 8 * nb_edges)) / 2)
+
+
 def laplacian(m: np.ndarray[int]) -> np.ndarray[int]:
     """Returns the laplacian of a graph given by its adjacency matrix.
 
@@ -82,3 +86,26 @@ def make_matrix(state: np.ndarray[int], n: int) -> np.ndarray[int]:
         m[i + 1 :, i] = state[k : k + n - i - 1]
         k += n - i - 1
     return m
+
+
+def make_matrix_from_string(s: str) -> np.ndarray[int]:
+    return make_matrix([int(x) for x in s], get_nb_vertices(len(s)))
+
+
+def improve_graph(state, n, reward_function):
+    m = n * (n - 1) // 2
+    best_reward = reward_function(state, n)
+    best_state = state.copy()
+    did_improve = False
+    for i in range(m):
+        state[i] = 1 - state[i]
+        reward = reward_function(state, n)
+        if reward > best_reward:
+                best_reward = reward
+                best_state = state.copy()
+                did_improve = True
+        state[i] = 1 - state[i]
+    if did_improve:
+        return improve_graph(best_state, n, reward_function)
+    else:
+        return best_state, best_reward
